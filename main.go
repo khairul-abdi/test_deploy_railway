93b7bbc50a7fed3a4be100e942a3a9fc25aa3e56
Khairul Abdi <khairul.abdi@koinworks.com>
2024-05-12 11:10:44 +0700

    add log

--- main.go
+++ main.go
@@ -11,5 +11,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
+	fmt.Println("server started at localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
